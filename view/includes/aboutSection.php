b6955b9ff2c3cb7f9879239373d1a64cf2360951
KamillaHS <kamilla.strobaek@gmail.com>
2019-05-10 15:26:43 +0200

    Rules and Reg and About written and put into DB script

--- view/includes/aboutSection.php
+++ view/includes/aboutSection.php
@@ -4,12 +4,13 @@
         <p>
             <?php
             $user_id = $_SESSION['user_id'];
-            $sql = "SELECT Description FROM websiteinfo";
+            $sql = "SELECT Description, Contact FROM websiteinfo";
             $query = $dbCon->prepare($sql);
             $query->execute();
             $getAbout= $query->fetch();
             echo "$getAbout[0]";
             ?>
+            <i><?php echo $getAbout['Contact'] ?></i>
         </p>
     </div>
 </div>
